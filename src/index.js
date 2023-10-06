8a1ad12dbbf3cc3d3c80b047a247ca381d0cd504
Dzima Bykau <bykovdmitry3@gmail.com>
2023-10-06 09:57:10 +0300

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = [];
+    let nArr = n.toString().split("");
+    for (let i = 0; i < nArr.length; i++) {
+        result.unshift(nArr[i]);
+    }
+    return parseInt(result.join(""));
+};
